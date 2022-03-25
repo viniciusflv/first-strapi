b5c0af2cb17673d63fd24cfb5b54b30ad67cd2e2
Vinicius Victorino <viniciusflv@gmail.com>
2022-03-25 11:10:41 -0300

    feat: config sync

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'bb3f0597014cc562acd7aa25d0259253'),
+    secret: env("ADMIN_JWT_SECRET", "bb3f0597014cc562acd7aa25d0259253"),
   },
+  watchIgnoreFiles: ["**/config/sync/**"],
 });
