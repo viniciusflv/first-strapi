2a03dcc8beba884a585bc76cd87b4ec8efb2b159
Vinicius Victorino <viniciusflv@gmail.com>
2022-04-29 10:17:49 -0300

    chore: add morphism to simplify api

--- src/admin/extensions/themes/colors.js
+++ src/admin/extensions/themes/colors.js
@@ -1,94 +1,87 @@
-const appColors = {
-  primaryColor: "#FAE22C",
-  secondaryColor: "#465EFF",
-  fontSecondaryColor: "#465EFF",
-  backgroundBlueColor: "#465EFF",
-  terciaryBlue: "#465EFF",
-  darkBlue: "#465EFF",
-  lightBlue: "#465EFF",
-  secondaryLightBlue: "#465EFF",
-  terciaryLightBlue: "#4ADBFD",
-  dividerLightBlue: "#DDE4FC",
-  sideMenuLightBlue: "#F3EEFB",
-  bgBlue: "#3751FF",
-  secondaryBlue: "#2E49FD",
-  fontLightBlue: "#83ACFF",
-  fontTextBlue: "#8891CC",
-  whiteColor: "#FFFFFF",
-  backgroundColor: "rgba(70, 94, 255, 0.03)",
-  backgroundColorLight: "#FAFAFA",
-  greenWhats: "#43A047",
-  greenCard: "#7effe91a",
-  greenCardBorder: "#00EBCF",
-  dividerBlue: "#66869B",
-  lightGray: "#F2F9FF",
-  borderLightGray: "#E7EDFB",
-  secondaryLightGray: "#E8F3FC",
-  terciaryLightGray: "#E0E7FF",
-  paperLightGreen: "#7EFFE9",
-  imageLightGreen: "#C3FFF4",
-  paperLightGray: "#E5E5E5",
-  black: "#000000",
-  menuYellow: "#FCEE4F",
-  secondaryYellow: "#FFED00",
-  terciaryYellow: "#FAE128",
-  fontBlack: "#4C4C4C",
-  colorGray: "#4E4E4E",
-  secondaryGray: "#474747",
-  terciaryGray: "#8798AD",
-  alternativeGray: "rgba(120, 120, 120, 0.38)",
-  primaryRed: "#E11919",
-  redButton: "#E53935",
-  redStatus: "#F97A70",
-  blackInativo: "#F5F5F5",
-  backgroundGroup: "#F9F7FF",
-  borderGroup: "#DDE4FC",
-};
+import { morphism, createSchema } from "morphism";
 
-export const colors = {
-  neutral100: "#f5f5f5", // primaryBackground
-  neutral0: "#fff", // secondaryBackground
-  buttonNeutral0: "#fff", // buttonText
-  neutral300: "#66339930", // stepDivider
-  neutral150: "#f5f5f5", // Divider InputBackground
-  neutral200: "#66339930", // Divider
-  primary200: "#66339930", // lightPrimary
-  neutral500: "#0008", // menuInactiveIcons
-  neutral600: "#0008", // textSecondary
-  neutral800: "#000", // textPrimary
-  secondary100: "#66339910", // editBackground
-  primary100: "#66339910", // activeItemBackground
-  primary600: "#663399", // activeText
-  primary700: "#663399", // activeText
-  primary500: "#663399", // activeStepDivider
-  neutral700: "#663399", // hoverTextMenuItem
-  neutral400: "#663399", // arrowActive
-  secondary200: "#663399", // editBorder
-  secondary700: "#663399", // editText
-  secondary600: "#663399", // editBullet
-  neutral900: "#663399", // tooltip Icon?
-  danger100: "#FFB8B8", // alertBackground
-  danger600: "#FF5252", // focusDanger
-  danger200: "#f00", // alertBorder
-  danger700: "#f00", // alertText
-  success100: "#B8FFB8", // successBackground
-  success200: "#008F00", // successBorder
-  success600: "#008F00", // successBullet
-  success700: "#008F00", // successText
-  secondary500: "#f0f", // ?
-  danger500: "#f0f", // ?
-  success500: "#f0f", // ?
-  neutral1000: "#f0f",
+export const colors = morphism(
+  createSchema(
+    {
+      neutral0: "background.card",
+      neutral100: "background.main",
+      danger100: "background.danger",
+      danger600: "background.danger",
+      primary100: "background.active",
+      success100: "background.success",
+      secondary100: "background.active",
 
-  alternative100: "#f00",
-  alternative200: "#f00",
-  alternative500: "#f00",
-  alternative600: "#f00",
-  alternative700: "#f00",
+      neutral800: "text.main",
+      danger700: "text.danger",
+      primary600: "text.active",
+      primary700: "text.active",
+      neutral700: "text.active",
+      success700: "text.success",
+      neutral600: "text.neutral",
+      secondary700: "text.active",
+      buttonNeutral0: "text.button",
 
-  warning100: "#ffdc12",
-  warning200: "#ffdc12",
-  warning500: "#ffdc12",
-  warning600: "#ffdc12",
-  warning700: "#ffdc12",
-};
+      neutral300: "border.main",
+      neutral200: "border.main",
+      primary200: "border.main",
+      danger200: "border.danger",
+      primary500: "border.active",
+      success200: "border.success",
+      neutral150: "border.neutral",
+      secondary200: "border.active",
+
+      neutral900: "icon.active",
+      neutral400: "icon.active",
+      success600: "icon.success",
+      neutral500: "icon.neutral",
+      secondary600: "icon.active",
+
+      secondary500: "",
+      danger500: "",
+      success500: "",
+      neutral1000: "",
+      alternative100: "",
+      alternative200: "",
+      alternative500: "",
+      alternative600: "",
+      alternative700: "",
+      warning100: "",
+      warning200: "",
+      warning500: "",
+      warning600: "",
+      warning700: "",
+    },
+    {
+      undefinedValues: { strip: true },
+    }
+  ),
+  {
+    icon: {
+      main: "#0008",
+      active: "#663399",
+      success: "#008F00",
+    },
+    text: {
+      main: "#000",
+      neutral: "#0008",
+      active: "#663399",
+      button: "#fff",
+      danger: "#f00",
+      success: "#008F00",
+    },
+    border: {
+      main: "#66339950",
+      neutral: "#f5f5f5",
+      active: "#663399",
+      danger: "#f00",
+      success: "#008F00",
+    },
+    background: {
+      main: "#f5f5f5",
+      active: "#66339910",
+      card: "#fff",
+      danger: "#FFB8B8",
+      success: "#B8FFB8",
+    },
+  }
+);
